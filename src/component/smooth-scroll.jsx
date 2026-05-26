20ee28a0173f27112d57b4d7b12af46224b354dc
Hrithik <hrithiknonia66@gmail.com>
2026-05-26 14:33:06 +0530

    smooth scroll with lenis add

---
+++ src/component/smooth-scroll.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import Lenis from "lenis";
+import "lenis/dist/lenis.css";
+
+export default function SmoothScroll({ children }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
+  return children;
+}
---
+++ src/constant.js
@@ -0,0 +1,6 @@
+export const links = [
+  { link: "/", text: "Home" },
+  { link: "/services", text: "Services" },
+  { link: "/gallary", text: "Gallary" },
+  { link: "/contact", text: "Contact" }
+];
