5f78a1b03a1c68d0edcbc11fb2e04a430a5bd9a3
Hrithik <hrithiknonia66@gmail.com>
2026-05-28 18:22:08 +0530

    service page complete

--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -21,7 +21,7 @@ export default function Services() {
   return (
     <>
       {/* first section */}
-      <div className="w-full  bg-blue-500 lg:px-10 lg:py-35 py-20 px-3 md:py-30 md:px-5">
+      <div className="w-full lg:px-10 lg:py-35 py-20 px-3 md:py-30 md:px-5">
         {/* heading */}
         <div className="lg:flex lg:justify-between  pb-10">
           <h1 className="lg:text-5xl text-black/90 text-2xl mb-2 md:text-3xl">
@@ -34,13 +34,16 @@ export default function Services() {
 
         <div className=" lg:grid-cols-3 gap-y-5 flex flex-col md:grid md:grid-cols-2 md:gap-4">
           {/* content cards*/}
-          {tempCardData.map(({ img, heading, disc }) => (
-            <div className="max-w-sm rounded-2xl overflow-hidden bg-white border border-gray-200 shadow-sm transition-transform duration-200 hover:scale-103 hover:shadow-xl">
+          {tempCardData.map(({ img, heading, disc }, i) => (
+            <div
+              className="max-w-sm rounded-2xl overflow-hidden bg-white border border-gray-200 shadow-sm transition-transform duration-200 hover:scale-103 hover:shadow-xl"
+              key={i}
+            >
               {/* Image */}
               <div className="w-full aspect-[4/3] overflow-hidden">
                 <img
                   src={img}
-                  alt="Luxury car interior showing leather door panel"
+                  alt={`${heading} image`}
                   className="w-full h-full object-cover"
                 />
               </div>
@@ -58,8 +61,27 @@ export default function Services() {
       </div>
 
       {/* second section */}
-      <div className="w-full bg-green-500 ">
-        <p className="text-white text-xl font-semibold">2</p>
+      <div className="w-full bg-green-500 relative h-[550px]">
+        <img
+          src="../../public/servise-page-img/banner-img.avif"
+          alt="Service Detail"
+          className=" h-full w-full object-cover object-[25%_center] md:object-center"
+        />
+
+        {/* banner text */}
+        <div className=" absolute  inset-0 bg-black/10 text-white flex flex-col justify-center px-5 md:w-[60%] md:px-10 lg:w-1/2">
+          <h1 className="text-2xl mb-2 md:text-4xl md:mb-5 font-semibold">
+            Enhance Your Protection
+          </h1>
+          <p className="leading-5 md:text-2xl md:leading-8">
+            enhance your protection Elevate your detailing experience with
+            specialized services like wheel coating, glass polishing, and engine
+            bay detailing
+          </p>
+          <button className="mt-6 w-fit bg-white text-black font-semibold px-6 py-3 rounded-md hover:bg-white/60 transition">
+            Get a Quote
+          </button>
+        </div>
       </div>
     </>
   );
